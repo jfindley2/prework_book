aea2b4226dce3cae436d07939360f85ab6898b1b
Jacob Findley <jfindley2@cnm.edu>
2015-12-17 12:44:57 -0700

    Added test file

--- testing.php
+++ testing.php
@@ -1,4 +1,9 @@
-<?php
-	echo "This is just a test";
-?>
-
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>I completed the prework</title>
+	</head>
+	<body>
+		<p>I completed the <b>prework!</b></p>
+	</body>
+</html>
